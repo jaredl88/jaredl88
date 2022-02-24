fcbf29b25f612725232314bfe23b9b6de15234f9
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-02-24 17:42:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const addTaskPath = '/addtask';
 const getTaskPath = '/gettasks';
 const todoService = require('./service/addtodo');
 const getTaskService = require('./service/getTasks');
+const deleteTaskPath = '/deletetask';
+const deleteService = require('./service/deleteTask');
 
 exports.handler = async (event) => {
   console.log('Request Event: ', event);
@@ -19,6 +21,10 @@ exports.handler = async (event) => {
       break;
     case event.httpMethod === 'GET' && event.path === getTaskPath:
        response = await getTaskService.getTask();
+      break;
+    case event.httpMethod === 'POST' && event.path === deleteTaskPath:
+      const deleteBody = JSON.parse(event.body);
+      response = await deleteService.deleteTask(deleteBody);
       break;
     default:
       response = util.buildResponse(404, '404 Not Found');
