84c2989af70069def426a4b5e157cb5a83b1652e
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-02-24 10:47:04 -0600

    Update getTasks.js
    Made it so it would work correctly with api displaying tasks when called. Made it to go through the util for the proper headers

--- service/getTasks.js
+++ service/getTasks.js
@@ -10,6 +10,20 @@ const userTable = 'todoUsers';
 
 
 async function getTask(){
+    
+    const getResponse = await getTasks();
+    if(!getResponse)
+    {
+        return util.buildResponse(503,{
+           message: 'Server Error, Please try again later'
+    })
+    }
+    return util.buildResponse(200,{
+          getResponse
+});
+
+}
+async function getTasks(){
     const userName = 'jared';
     const userTable = 'todoUsers';
     try {
@@ -21,7 +35,7 @@ async function getTask(){
             TableName: userTable
         };
         var result = await dynamodb.query(params).promise()
-        return JSON.stringify(result);
+        return JSON.stringify(result.Items);
     } catch (error) {
         console.error(error);
     }
