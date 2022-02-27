a9874c9bc7e25d14776874450510586585566309
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-02-26 18:34:07 -0600

    Update register.js
    added user check

--- service/register.js
+++ service/register.js
@@ -1,3 +1,4 @@
+
 const AWS = require('aws-sdk');
 
 AWS.config.update({
@@ -6,67 +7,88 @@ AWS.config.update({
 })
 const util = require('../utils/util');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const userTable = 'AppUsers';
-const bcrypt = require('bcryptjs');
+const userTable = 'todoLogin';
+const bcryptjs = require('bcryptjs');
 
-async function register(userInfo){
-    const name = userInfo.name;
-    const email = userInfo.email;
-    const username = userInfo.username;
-    const password = userInfo.password;
-    if (!name || !username || !email || !password){
-        return util.buildResponse(401,{
+async function register(event){
+    const userName = event.username;
+    const userEmail = event.email;
+    const userPassword = event.password;
+
+    if(!userName || !userEmail || !userPassword)
+    {
+         return util.buildResponse(401,{
             message: 'You are missing one or more required feilds'
-        } )
+        })
+
     }
-    const dUser = await getUser(username.toLowerCase().trim());
+    const dUser = await getUser(userName.toLowerCase().trim());
     if(dUser && dUser.username){
         return util.buildResponse(401,{
             message: 'Username is already being used, choose a different username'
         })
     }
-    const encryptedPassword = bcrypt.hashSync(password.trim(0), 10);
-    const user = {
-        name: name,
-        email: email,
-        username: username.toLowerCase().trim(),
-        password: encryptedPassword
-
-    }
-    const saveUserResponse = await saveUser(user);
-    if(!saveUserResponse){
+    const saltRounds = 10;
+    const hashpassword = await bcryptjs.hash(userPassword, saltRounds);
+    
+      const user = {
+      username: userName,
+      useremail: userEmail,
+      password: hashpassword
+  }  
+ 
+   const saveTaskResponse = await saveUser(user);
+   if(!saveTaskResponse){
         return util.buildResponse(503,{
-            message: 'Server Error, Please try again later'
+           message: 'Server Error, Please try again later'
         })
-    }
+   }
     return util.buildResponse(200, {
-        username: username
-    });
-}
-
-async function getUser(username){
-const params = {
-    TableName: userTable,
-    Key: {
-        username: username
-    }  
-}
-return await dynamodb.get(params).promise().then(response => {
-    return response.Item;
-}, error =>{
-    console.log('There is an error getting user: ', error);
-})
-}
-
-async function saveUser(user){
+        userName
+   });
+   
+   async function getUser(userName){
     const params = {
         TableName: userTable,
-        Item: user
+        Key: {
+            "username": userName
+        }  
     }
-    return await dynamodb.put(params).promise().then(() =>{
-        return true;
+    return await dynamodb.get(params).promise().then(response => {
+        return response.Item;
     }, error =>{
-        console.error('There is an error saving user', error);
-    });
+        console.log('There is an error getting user: ', error);
+    })
+    }
+ async function saveUser(user){
+ 
+   
+    const params = {
+            TableName: userTable,
+            Key: {
+                "username": user.username
+            },
+            UpdateExpression: "SET #UserName = :val, #UserEmail = :valE, #UserPassword = :valP",
+            ExpressionAttributeNames: {
+                "#UserName": "Username",
+                "#UserEmail": "Email",
+                "#UserPassword": "Password"
+            },
+            ExpressionAttributeValues: {
+                ":val": user.username,
+                ":valE": user.useremail,
+                ":valP": user.password
+            }
+        };
+    return await dynamodb.update(params).promise().then(response => {
+ return true;
+}, error =>{
+    console.log('There is an error getting task: ', error);
+});
+   
+ }
+
+ 
 }
-module.exports.register= register;
+
+module.exports.register = register;
