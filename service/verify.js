0ee5e1e08498475ceaccee710ae1fb2ba8258a78
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-02-28 11:54:37 -0600

    Update verify.js
    added comments

--- service/verify.js
+++ service/verify.js
@@ -2,6 +2,7 @@ const util = require('../utils/util');
 const auth = require('../utils/auth');
 
 function verify(requestBody){
+    //Checks to make sure required info is present in the request body
     if(!requestBody.user || !requestBody.user.username || !requestBody.token) { return util.buildResponse(401, {
         verified: false,
         message: 'incorrect request body'
@@ -11,9 +12,11 @@ function verify(requestBody){
     const user = requestBody.user;
     const token = requestBody.token;
     const verification = auth.verifyToken(user.username, token);
+    //if information does not verify errors leaving verified as false
     if (!verification.verified) {
         return util.buildResponse(401, verification);
     }
+    //If it verfys makes verified true and returns the user message and token and true value
     return util.buildResponse(200, {
         verified: true,
         message:'verified',
