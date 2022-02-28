d5469a76771574ecc5a36a8781e1c6d2a827ceb0
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-02-28 10:34:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,32 +18,40 @@ exports.handler = async (event) => {
   console.log('Request Event: ', event);
   let response;
   switch(true){
+      //Case to test whether API is online or not
     case event.httpMethod === 'GET' && event.path === healthPath: 
       response = util.buildResponse(200);
       break;
+      //Case for addtask method in API 
     case event.httpMethod === 'POST' && event.path === addTaskPath:
       const addtodoBody = JSON.parse(event.body);
       response = await todoService.addtodo(addtodoBody);
       break;
+       //Case for gettask method in API  
     case event.httpMethod === 'GET' && event.path === getTaskPath:
        response = await getTaskService.getTask();
       break;
+       //Case for deletetask method in API  
     case event.httpMethod === 'POST' && event.path === deleteTaskPath:
       const deleteBody = JSON.parse(event.body);
       response = await deleteService.deleteTask(deleteBody);
       break;
+      //Case for register method in API  
     case event.httpMethod === 'POST' && event.path === registerPath:
       const registerBody = JSON.parse(event.body);
       response = await registerService.register(registerBody);
       break;  
+      //Case for login method in API  
     case event.httpMethod === 'POST' && event.path === loginPath:
         const loginBody = JSON.parse(event.body);
         response = await loginService.login(loginBody);
         break; 
+      //Case for verify method in API  
     case event.httpMethod === 'POST' && event.path === verifyPath:
           const verifyBody = JSON.parse(event.body);
           response = verifyService.verify(verifyBody);
-          break;  
+          break;
+      //If path is not found 404 error
     default:
       response = util.buildResponse(404, '404 Not Found');
   }
