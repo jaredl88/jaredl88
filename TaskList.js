f31b54dca97be346d3099d9419efc6d61b2f11e1
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-03-08 18:40:46 -0600

    Update TaskList.js

--- TaskList.js
+++ TaskList.js
@@ -7,64 +7,54 @@ const apiUrl =  'https://j2b30glms2.execute-api.us-east-1.amazonaws.com/prod/get
     
     
 function TaskList () {
-    const [getResult, setGetResult] = useState('');
+    const [getResult, setGetResult] = useState([]);
     const [errorMessage, setErrorMessage] = useState(null);
     
+    useEffect(() => {
+      fetchData();
     
+      }, []);
 
-    
-    
-    
-  /*  axios.get(apiGetUrl, requestConfig).then((response) => {
-         console.log(response);
-         var result =[];
-         result = JSON.parse(response.data);
-         console.log(result);
-        setGetResult (result.map( tasks => ({value: tasks.taskName, text: tasks.taskText})));
-         }).catch((error) => {
-           console.log(error);
-           if (error.response.status === 401 || error.response.status === 403){
-           setErrorMessage(error.response.data.message);
-           }
-           else {
-               setErrorMessage('sorry ... the server is down! please try again later');
-           }
-       }) */
-    
+
        useEffect(() => {
-        const requestConfig = {
-          //store as envirnment variable later
-          headers: {
-         'x-api-key': '341SCwBxjy6KSOErzBmCI4mteixN90yC7L8OD37O'
-          }
-        }
-       
-        //Uses verify api path to verify token sets authenicating to false when authentication is done if verification does 
-        //not work resets user session
-        axios.get(apiUrl,requestConfig).then(response => {
-            console.log(response);
-          setGetResult(response.data);
-        }).catch((error) => {
-            if (error.response.status === 401 || error.response.status === 403){
-            setErrorMessage(error.response.message);
-            }
-            else {
-                setErrorMessage('sorry ... the server is down! please try again later');
-            }
-        })
+        console.log(getResult);
       
-        }, []);
+        }, [getResult]);
 
-return(
-    
-        <div className="TaskList">
-            <div>
-              {getResult}
-       </div>
-       </div>
-      
-        )   
-              
+        const fetchData = async () => {
+          const requestConfig = {
+              //store as envirnment variable later
+              headers: {
+             'x-api-key': '341SCwBxjy6KSOErzBmCI4mteixN90yC7L8OD37O'
               }
+            }
+          const response = await axios.get(apiUrl,requestConfig);
+          if(!response){
+              setErrorMessage('There was an error retrieving tasks')
+          }
+          setGetResult(response.data.getResponse.Items[0])
+         }
+console.log(getResult);
+const taskArray = Object.entries(getResult)
+  console.log(taskArray);
+        return(
+   
+          <div className="TaskList">
+             {
+           
+          taskArray && taskArray.map(taskArrays =>{
+            return(
+              <div key={taskArrays[0]} style={{alignItems:'center',margin:'20px 60px'}}>
+              <h4>{taskArrays[0]}: {taskArrays[1]}</h4> <br />
+            </div>
+            )
+          })
+                 
+  }
+         </div>
+       
+          );
+  
+                }
 
 export default TaskList;
