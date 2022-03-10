009e7618ada80c19c3ddf73458e49820a2737801
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-03-09 18:44:40 -0600

    Update TaskList.js
    Added complete button to call delete api method for each task

--- TaskList.js
+++ TaskList.js
@@ -1,31 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import "./TaskList.css";
-import render from "react-dom";
 import {getUser, resetUserSession} from './service/auth';
-const apiUrl =  'https://j2b30glms2.execute-api.us-east-1.amazonaws.com/prod/gettasks';
-    
-    
+const apiUrl =  '';
+const apiDeleteUrl =    '';
+   
 function TaskList () {
     const [getResult, setGetResult] = useState([]);
     const [errorMessage, setErrorMessage] = useState(null);
-    
+    const [message, setMessage] = useState(null);
+    const [tName, setTName] = useState('');
+
+    const deleteTask = (event)=>{
+      event.preventDefault();
+      
+      const requestConfig = {
+        //store as envirnment variable later
+        headers: {
+       'x-api-key': '341SCwBxjy6KSOErzBmCI4mteixN90yC7L8OD37O'
+        }
+      }
+      const requestBody = {
+        tName: tName
+      }
+        const response = axios.post(apiDeleteUrl, requestBody, requestConfig);
+        if(!response){
+          setErrorMessage('There was an error retrieving tasks')
+      }
+      setMessage('')
+     
+    }
     useEffect(() => {
       fetchData();
-    
+   
       }, []);
 
 
        useEffect(() => {
         console.log(getResult);
-      
+     
         }, [getResult]);
 
         const fetchData = async () => {
           const requestConfig = {
               //store as envirnment variable later
               headers: {
-             'x-api-key': ''
+             'x-api-key': '341SCwBxjy6KSOErzBmCI4mteixN90yC7L8OD37O'
               }
             }
           const response = await axios.get(apiUrl,requestConfig);
@@ -34,27 +54,32 @@ function TaskList () {
           }
           setGetResult(response.data.getResponse.Items[0])
          }
-console.log(getResult);
-const taskArray = Object.entries(getResult)
+    console.log(getResult);
+    const taskArray = Object.entries(getResult)
   console.log(taskArray);
         return(
    
           <div className="TaskList">
              {
            
-          taskArray && taskArray.map(taskArrays =>{
+          taskArray && taskArray.map(taskArrays =>{ if (taskArrays[0] !== 'username'){
             return(
+              <form onSubmit={deleteTask}>
               <div key={taskArrays[0]} style={{alignItems:'center',margin:'20px 60px'}}>
-              <h4>{taskArrays[0]}: {taskArrays[1]}</h4> <br />
+              <h4>{taskArrays[0]}: {taskArrays[1]} <button type="submit" value="submit" onClick={ event=> setTName(taskArrays[0].toString())}>Complete Task</button></h4><br />
+          
             </div>
+            </form> 
+             
             )
-          })
+           }})
+          
                  
   }
          </div>
        
           );
-  
+ 
                 }
 
 export default TaskList;
