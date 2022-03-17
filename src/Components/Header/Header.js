4fe3cdef3e32f863039df0c22c1090811641265e
jaredl88 <59318633+jaredl88@users.noreply.github.com>
2022-03-17 18:46:39 -0500

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch, NavLink } from "react-router-dom";
 //import react pro sidebar components
-import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
-} from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
-import "./Header.css";
-import { BiLogInCircle } from "react-icons/bi";
-import { MdPlaylistAdd } from "react-icons/md";
-import { FaList } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
-import { IoIosCheckmarkCircleOutline } from "react-icons/io";
-import { GrView } from "react-icons/gr";
-import { GiArchiveRegister } from "react-icons/gi";
+
+
+
+
 import Home from "../../Home";
 import Login from "../../Login";
 import Todo from "../../addtask";
@@ -28,71 +15,57 @@ import TaskList from "../../TaskList";
 import { resetUserSession } from "../../service/auth";
 import CompletedTaskList from "../../getCompleted";
 const Header = () => {
-  
+ 
     //create initial menuCollapse state using useState hook
-    const [menuCollapse, setMenuCollapse] = useState(false)
 
-    //create a custom function that will change menucollapse state from false to true and true to false
-  const menuIconClick = () => {
-    //condition checking to change state from true to false and vice versa
-    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-  };
     const Logout = () =>{
       resetUserSession();
     }
   return (
     <>
-    <div>
     <BrowserRouter>
-      <div id="header">
-          {/* collapsed props to change menu size using menucollapse state */}
-        <ProSidebar collapsed={menuCollapse}>
-          <SidebarHeader>
-          <div className="logotext">
-              {/* small and big change using menucollapse state */}
-              <p>{menuCollapse ? "Tasks" : "Tasks"}</p>
-            </div>
-            <div className="closemenu" onClick={menuIconClick}>
-                {/* changing menu collapse icon on click */}
-              {menuCollapse ? (
-                <FiArrowRightCircle/>
-              ) : (
-                <FiArrowLeftCircle/>
-              )}
-            </div>
-          </SidebarHeader>
-          
-          <div className ="header">
-          <SidebarContent>
-            <Menu iconShape="square">
-              <MenuItem active={window.location.pathname === "/"} icon={<FiHome />}>
-                Home <Link to="/" />
-              </MenuItem>
-              <MenuItem active={window.location.pathname === "/Login/"}icon={<BiLogInCircle  />}> Login <Link to="/Login"/></MenuItem>
-              <MenuItem active={window.location.pathname === "/Register/"}icon={<GiArchiveRegister />}>Register<Link to="/Register"/></MenuItem>
-              <MenuItem active={window.location.pathname === "/TaskList/"}icon={<FaList />}>Task List<Link to="/TaskList"/></MenuItem>
-              <MenuItem active={window.location.pathname === "/addtask/"} icon={<MdPlaylistAdd />}>Add New Task<Link to="/addtask"/></MenuItem>
-              <MenuItem active={window.location.pathname === "/getCompleted/"}icon={<GrView />}>Completed<Link to="/getCompleted"/></MenuItem>
-            </Menu>
-          </SidebarContent>
-          <SidebarFooter>
-            <Menu iconShape="square">
-              <MenuItem icon={<FiLogOut />} onClick={Logout}>Logout<Link to="/Login"/></MenuItem>
-            </Menu>
-          </SidebarFooter>
-          </div>
-        </ProSidebar>
+    <div class="top-9">
+   <div class="navbar bg-base-100" >
+  <div class="flex-1">
+    <NavLink exact activeClassName="active" class="btn btn-ghost upper-case text-xl" to="/">Home</NavLink>
+  </div>
+  <div class="flex-none visible hover:invisible">
+    <ul class="menu menu-horizontal p-0">
+      <li tabindex="0">
+        
+        <a>
+        
+          Links
+          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+      
+        </a>
+        
+        <ul class="p-2 bg-base-100 inset-y-7 visible">
+          <li><NavLink exact activeClassName="active" to="/Login">Login</NavLink></li>
+          <li><NavLink exact activeClassName="active" to="/Register">Register</NavLink></li>
+          <li><NavLink exact activeClassName="active" to="/addtask">Add Task</NavLink></li>
+          <li><NavLink exact activeClassName="active" to="/TaskList">Task List</NavLink></li>
+          <li><NavLink exact activeClassName="active" to="/getCompleted">Completed Tasks List</NavLink></li>
+        </ul>
+      </li>
+    </ul>
+    </div>
+    <div class="flex-1 right-0">
+      <button class="btn" onClick={Logout}>Logout</button>
       </div>
-      <div className="content">
+  </div>
+  </div>
+  <div className="content ">
+    <Switch>
             <Route exact path="/" component={Home}/>
             <PublicRoute exact path="/register" component={Register}/>
             <PublicRoute exact path="/login" component={Login}/>
             <PrivateRoute exact path="/addtask" component={Todo}/>
             <PrivateRoute exact path="/TaskList" component={TaskList}/>
             <PrivateRoute exact path="/getCompleted" component={CompletedTaskList}/>
+            </Switch>
           </div>
-          </BrowserRouter>
-          </div>
+</BrowserRouter>
     </>
   );
 };
